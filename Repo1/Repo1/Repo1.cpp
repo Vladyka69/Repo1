3a0eaa51842f3b57e19f0127e37e77b2af4cc1be
PIBO0122010 Котов Сергей Владимирович <PIBO0122010@sibupk.su>
2024-02-10 20:27:45 +0700

    Добавление репозитория

---
+++ Repo1/Repo1/Repo1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "rus");
+    int year;
+
+    cout << "Введите год: ";
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                cout << year << " Год високосный ";
+            else
+                cout << year << " Год не високосный ";
+        }
+        else
+            cout << year << " Год високосный ";
+    }
+    else
+        cout << year << " Год не високосный";
+
+    return 0;
+}
